696fae2f105786a09fd9d0902a3fdff3fb8246db
abdellahmuhammed <abdellah.mohamedcs@gmail.com>
2022-04-17 21:10:57 +0200

    fetch model

--- lib/models/GetDataUser/GetUserDataModel.dart
+++ lib/models/GetDataUser/GetUserDataModel.dart
@@ -1,61 +1,34 @@
 class GetUserDataModel {
-  String token;
-  int userId;
-  dynamic roleId;
-  dynamic userRole;
   List<Data> data;
   bool success;
 
-  GetUserDataModel(
-      {this.token,
-        this.userId,
-        this.roleId,
-        this.userRole,
-        this.data,
-        this.success});
+  GetUserDataModel({this.data, this.success});
 
   GetUserDataModel.fromJson(Map<String, dynamic> json) {
-    token = json['token'];
-    userId = json['user_id'];
-    roleId = json['role_id'];
-    userRole = json['user_role'];
     if (json['data'] != null) {
-      data = List<Data>();
+      data = <Data>[];
       json['data'].forEach((v) {
         data.add(new Data.fromJson(v));
       });
     }
     success = json['success'];
   }
-
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['token'] = this.token;
-    data['user_id'] = this.userId;
-    data['role_id'] = this.roleId;
-    data['user_role'] = this.userRole;
-    if (this.data != null) {
-      data['data'] = this.data.map((v) => v.toJson()).toList();
-    }
-    data['success'] = this.success;
-    return data;
-  }
 }
 
 class Data {
-  String iD;
+  int iD;
   String username;
   String gender;
-  String dob;
+  Null dob;
   String job;
   String address;
   String email;
-  String phoneNum;
+  int phoneNum;
   String socialStatus;
-  String ssn;
-  String noOfKids;
+  int ssn;
+  Null noOfKids;
   String coordinates;
-  String roleId;
+  int roleId;
   String vaccinationType;
 
   Data(
@@ -91,22 +64,4 @@ class Data {
     vaccinationType = json['vaccination_type'];
   }
 
-  Map<String, dynamic> toJson() {
-    final Map<String, dynamic> data = new Map<String, dynamic>();
-    data['ID'] = this.iD;
-    data['username'] = this.username;
-    data['gender'] = this.gender;
-    data['dob'] = this.dob;
-    data['job'] = this.job;
-    data['address'] = this.address;
-    data['email'] = this.email;
-    data['phone_num'] = this.phoneNum;
-    data['social_status'] = this.socialStatus;
-    data['ssn'] = this.ssn;
-    data['no_of_kids'] = this.noOfKids;
-    data['coordinates'] = this.coordinates;
-    data['role_id'] = this.roleId;
-    data['vaccination_type'] = this.vaccinationType;
-    return data;
-  }
 }
